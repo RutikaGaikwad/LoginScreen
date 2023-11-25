47271fb5db675ce5d2b7f178e689368fbd3b2f48
RutikaGaikwad <129734035+RutikaGaikwad@users.noreply.github.com>
2023-11-26 01:50:45 +0530

    Add files via upload

---
+++ Loginform/Loginform/LoginformApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoginformApp.swift
+//  Loginform
+//
+//  Created by APPLE on 26/11/23.
+//
+
+import SwiftUI
+
+@main
+struct LoginformApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
